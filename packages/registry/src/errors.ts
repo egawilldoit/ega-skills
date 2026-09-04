5bc18ddf5880b9fea570dc9d7ce90cfe11fafe16
MORTAKI0 <115146963+MORTAKI0@users.noreply.github.com>
2026-09-04 01:42:35 +0100

    feat(registry): implement SQLite foundation lifecycle

---
+++ packages/registry/src/errors.ts
@@ -0,0 +1,24 @@
+export const REGISTRY_ERROR_CODES = [
+  "E_REGISTRY_HOME",
+  "E_REGISTRY_DB_OPEN",
+  "E_REGISTRY_MIGRATION",
+  "E_REGISTRY_FTS5_UNAVAILABLE",
+  "E_REGISTRY_SCHEMA_NEWER",
+] as const;
+
+export type RegistryErrorCode = (typeof REGISTRY_ERROR_CODES)[number];
+
+export class RegistryError extends Error {
+  readonly code: RegistryErrorCode;
+
+  constructor(code: RegistryErrorCode, message: string, cause?: unknown) {
+    super(message);
+    this.name = "RegistryError";
+    this.code = code;
+    if (cause !== undefined) this.cause = cause;
+  }
+}
+
+export function isRegistryError(error: unknown): error is RegistryError {
+  return error instanceof RegistryError;
+}
